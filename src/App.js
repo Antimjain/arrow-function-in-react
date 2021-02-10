3cb1f8ddfc53f56949c334cd4ae807c6a2702f4d
antim-stream <antim@stream.space>
2021-02-10 19:02:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ButtonWithBind extends Component {
+  constructor() {
+    super();
+
+    this.state = { toggle: false };
+    // this.toggleButton = this.toggleButton.bind(this);
+  }
+
+  toggleButton() {
+    this.setState(prevState => ({ toggle: !prevState.toggle }));
+  }
+
+  toggleButton = () => { 
+    this.setState(prevState => ({ toggle: !prevState.toggle }));
+  }
+  
+  render() {
+    const toggle = this.state.toggle;
+
+    return (
+      <div>
+        <button onClick={this.toggleButton}>
+          {toggle ? "ON" : "OFF"}
+        </button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default ButtonWithBind;
